1cb2c224f222cc5273c73e6cbdb90dc585147122
hfox999 <37228013+hfox999@users.noreply.github.com>
2018-05-07 17:48:07 -0700

    SoundBoard Activity
    added activity with 30 buttons. When app is run, it opens directly to existing soundboard (fix later once we add other activities)

--- app/src/main/java/edu/ucsc/soundboard/soundboard.java
+++ app/src/main/java/edu/ucsc/soundboard/soundboard.java
@@ -3,13 +3,11 @@ package edu.ucsc.soundboard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import static edu.ucsc.soundboard.R.layout.activity_soundboard;
-
-public class MainActivity extends AppCompatActivity {
+public class soundboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(activity_soundboard);
+        setContentView(R.layout.activity_soundboard);
     }
 }
