4859a5bd232ac593e49bb16332dc8a61cdf4c5a3
lucas861223 <lucas861223@gmail.com>
2018-05-14 12:28:07 -0700

    Barebone UI
    A couple pages, haven't done much functionality

---
+++ app/src/main/java/edu/ucsc/soundboard/LoadBoardScreen.java
@@ -0,0 +1,17 @@
+package edu.ucsc.soundboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+public class LoadBoardScreen extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soundboard);
+    }
+}
--- app/src/main/java/edu/ucsc/soundboard/MainActivity.java
+++ app/src/main/java/edu/ucsc/soundboard/MainActivity.java
@@ -2,15 +2,31 @@ package edu.ucsc.soundboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
-import static edu.ucsc.soundboard.R.layout.activity_soundboard;
-import static edu.ucsc.soundboard.R.layout.activity_settings;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(activity_settings);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToSetting(View view){
+        Intent settingScreen = new Intent(getApplicationContext(), Settings.class);
+        startActivity(settingScreen);
+    }
+
+    public void makeNewBoard(View view){
+        Intent board = new Intent(getApplicationContext(), Soundboard.class);
+        board.putExtra("boardjson", Soundboard.emptyBoardJSON().toString());
+        startActivity(board);
+    }
+
+    public void loadBoard(View view){
+        Intent settingScreen = new Intent(getApplicationContext(), Settings.class);
+        startActivity(settingScreen);
     }
 }
